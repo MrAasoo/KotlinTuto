06402f2e3f53a6bb35842212f5a7fc57e097a0ea
Himanshu Srivastava <110518743+himanshu-aasoo@users.noreply.github.com>
2023-07-24 13:03:31 +0530

    Data type

---
+++ src/main/kotlin/DataType.kt
@@ -0,0 +1,33 @@
+/**
+ * Created by Himanshu Srivastava on 24,July,2023
+ * Project KotlinTuto
+ */
+
+fun main(args: Array<String>) {
+    var fName : String = "Himanshu"
+    var sName : String = "Srivastava"
+
+    println(fName + " " + sName)
+
+
+
+
+    var a:Byte = -128;
+    var b:Short = 32767;
+    var c:Int = 1234325
+    var d:Long = 14735L
+    var e:Float = 245.365F
+    var f:Double = 3654258.0136754227773684
+    var g:Boolean = true
+    var h:Char = 'H'
+
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+    println(g)
+    println(h)
+
+}
