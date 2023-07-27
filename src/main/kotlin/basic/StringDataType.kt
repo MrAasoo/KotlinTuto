2bbd7426a370649e9334a101bbf2e390bd542fed
Himanshu Srivastava <110518743+himanshu-aasoo@users.noreply.github.com>
2023-07-27 13:10:10 +0530

    String data type

---
+++ src/main/kotlin/basic/StringDataType.kt
@@ -0,0 +1,27 @@
+package basic
+
+
+/**
+ * Created by Himanshu Srivastava on 27,July,2023
+ * Project KotlinTuto
+ */
+
+fun main(args: Array<String>) {
+
+    var a: String = "Hello"
+    var b: String = "Kotlin Language"
+    println(a)
+    println(a + b)
+    println(a + " " + b)
+    println(a + " " + b.plus("!"))
+
+    var c: Int = 25
+    println("Print other datatype method 1 : " + c)
+    println("Print other datatype method 2 : $c")
+
+    var length: Int = b.length
+    println("Length b : $length")
+
+    println(a.lowercase())
+    println(b.uppercase())
+}
